fe0da9c997eeb23bb5e842ecd7a173c9be525986
Farjey <cvet57@rambler.ru>
2023-06-22 03:44:23 +0300

    Добавлены анимации к ховеру. Сделал слайдер - переключение с помощью dot с простой анимацией

---
+++ dist/js/slider.js
@@ -0,0 +1,45 @@
+const sliderSlide = document.querySelectorAll('.reviews__list .slider__item');
+const slider = document.querySelector('.reviews__list');
+const sliderDots = document.querySelectorAll('.reviews__toggles .slider__toggle');
+//const sliderBtnNext = document.querySelector('');
+//const sliderBtnPrev = document.querySelector('');
+
+let sliderCount = 0;
+let sliderWidth;
+
+//sliderBtnNext.addEventListener('click', nextSlide);
+//sliderBtnPrev.addEventListener('click', prevSlide);
+
+function nextSlide () {
+    sliderCount++;
+    if (sliderCount >= sliderSlide.length) sliderCount = 0;
+
+    //rollSlider();
+    thisSlide(sliderCount);
+};
+
+function prevSlide () {
+    sliderCount--;
+    if (sliderCount < 0) sliderCount = sliderSlide.length - 1;
+
+    //rollSlider();
+    thisSlide(sliderCount);
+};
+
+function thisSlide(index) {
+    sliderDots.forEach(item => {
+        item.classList.remove('slider__toggle--current')
+    });
+    sliderSlide.forEach(item => {
+        item.classList.remove('slider__item--active')
+    });
+    sliderDots[index].classList.add('slider__toggle--current');
+    sliderSlide[index].classList.add('slider__item--active');
+};
+
+sliderDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        thisSlide(sliderCount);
+    });
+});
