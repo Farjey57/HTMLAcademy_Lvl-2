3def3c4e9e7c8d9405eec9449f1a38b0d966a6e4
Farjey <cvet57@rambler.ru>
2023-07-13 00:55:44 +0300

    завершил адаптив страницы формы. Добавил прелоадер. Добавил js для определения nav вкладки

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,20 @@
+"use strict"
 const pageHeader = document.querySelector('.page-header');
 const navToggle = document.querySelector('.main-nav__burger');
 const pageMain = document.querySelector('.page-main');
+const locPathName = document.location.pathname;
+const mainNavItems = document.querySelectorAll('.main-nav__link')
+
+
+function navItemActive (arr) {
+    for (const item of arr) {
+        if (item.pathname === locPathName) {
+            item.classList.add("main-nav__item--active")
+        }
+    } 
+}
+
+navItemActive(mainNavItems)
 
 navToggle.addEventListener('click', function() {
     if ( pageHeader.classList.contains('page-header--opened')) {
